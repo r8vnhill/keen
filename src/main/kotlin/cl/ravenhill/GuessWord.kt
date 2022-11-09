ba5a645d58f57a05327cacd9be131a1d7d850ac4
R8V <ignacio.slater@ug.uchile.cl>
2022-11-09 13:26:28 -0300

    DOC Guess word
    0.6.2211.7

--- src/main/kotlin/cl/ravenhill/GuessWord.kt
+++ src/main/kotlin/cl/ravenhill/GuessWord.kt
@@ -20,7 +20,7 @@ import cl.ravenhill.keen.operators.crossover.SinglePointCrossover
 /**
  * Documentation
  */
-fun fitnessFn(genotype: Genotype<Char>): Double {
+fun numberOfMatches(genotype: Genotype<Char>): Double {
     val target = "Sopaipilla"
     var fitness = 0.0
     genotype.chromosomes.forEach { chromosome ->
@@ -34,14 +34,14 @@ fun fitnessFn(genotype: Genotype<Char>): Double {
 }
 
 fun main() {
-    val engine = engine(::fitnessFn) {
+    val engine = engine(::numberOfMatches) {
         genotype = genotype {
-            chromosomes = listOf(CharChromosome.Builder(10))
+            chromosomes = listOf(CharChromosome.Builder(size = 10))
         }
         populationSize = 1000
         alterers = listOf(Mutator(0.03), SinglePointCrossover(0.06))
         limits = listOf(SteadyGenerations(10))
     }
     engine.evolve()
-    println(engine.statistics)
+    engine.statistics.forEach { println(it) }
 }
