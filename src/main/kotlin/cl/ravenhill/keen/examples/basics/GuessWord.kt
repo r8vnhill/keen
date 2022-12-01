41757f8b478cd0e6127e64baa697c3aee1ea38b3
R8V <ignacio.slater@ug.uchile.cl>
2022-12-01 00:11:09 -0300

    FEAT Rastrigin function
    v0.9.2211.7

--- src/main/kotlin/cl/ravenhill/keen/examples/basics/GuessWord.kt
+++ src/main/kotlin/cl/ravenhill/keen/examples/basics/GuessWord.kt
@@ -1,18 +1,9 @@
-/*
- * "Makarena" (c) by R8V.
- * "Makarena" is licensed under a
- * Creative Commons Attribution 4.0 International License.
- * You should have received a copy of the license along with this
- *  work. If not, see <https://creativecommons.org/licenses/by/4.0/>.
- */
-
-package cl.ravenhill
+package cl.ravenhill.keen.examples.basics
 
 import cl.ravenhill.keen.Builders.engine
 import cl.ravenhill.keen.Builders.genotype
 import cl.ravenhill.keen.genetic.Genotype
 import cl.ravenhill.keen.genetic.chromosomes.CharChromosome
-import cl.ravenhill.keen.limits.Match
 import cl.ravenhill.keen.limits.TargetFitness
 import cl.ravenhill.keen.operators.Mutator
 import cl.ravenhill.keen.operators.crossover.SinglePointCrossover
---
+++ src/main/kotlin/cl/ravenhill/keen/examples/optimization/RastriginFunction.kt
@@ -0,0 +1,45 @@
+/*
+ *
+ *  * "Keen" (c) by R8V.
+ *  * "Keen" is licensed under a
+ *  * Creative Commons Attribution 4.0 International License.
+ *  * You should have received a copy of the license along with this
+ *  *  work. If not, see <https://creativecommons.org/licenses/by/4.0/>.
+ *
+ */
+
+package cl.ravenhill.keen.examples.optimization
+
+import cl.ravenhill.keen.Builders.engine
+import cl.ravenhill.keen.Builders.genotype
+import cl.ravenhill.keen.genetic.Genotype
+import cl.ravenhill.keen.genetic.chromosomes.DoubleChromosome
+import cl.ravenhill.keen.operators.Mutator
+import cl.ravenhill.keen.operators.crossover.MeanCrossover
+import cl.ravenhill.keen.util.optimizer.FitnessMinimizer
+import cl.ravenhill.keen.util.statistics.StatisticCollector
+import cl.ravenhill.keen.util.statistics.StatisticPrinter
+import kotlin.math.cos
+
+private const val A = 10.0
+private const val R = 5.12
+private const val N = 2
+
+private fun fitness(x: Genotype<Double>): Double {
+    return A * N + x.chromosomes.first().genes.fold(0.0) { acc, gene ->
+        acc + gene.dna * gene.dna - A * cos(2 * Math.PI * gene.dna)
+    }
+}
+
+fun main() {
+    val engine = engine(::fitness, genotype {
+        chromosomes = listOf(DoubleChromosome.Builder(2, -R..R))
+    }) {
+        populationSize = 1000
+        optimizer = FitnessMinimizer()
+        alterers = listOf(Mutator(0.1), MeanCrossover(0.6))
+        statistics = listOf(StatisticCollector())
+    }
+    engine.run()
+    println(engine.statistics[0])
+}
