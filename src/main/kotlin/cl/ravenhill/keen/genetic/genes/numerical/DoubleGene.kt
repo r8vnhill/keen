2f6c8b31d8de8ce179b070906cb2558d28a9e1d2
R8V <ignacio.slater@ug.uchile.cl>
2022-12-08 19:39:30 -0300

    TEST Core, CharGene and DoubleGene
    v1.1.2212.14

--- src/main/kotlin/cl/ravenhill/keen/genetic/genes/numerical/DoubleGene.kt
+++ src/main/kotlin/cl/ravenhill/keen/genetic/genes/numerical/DoubleGene.kt
@@ -3,6 +3,7 @@ package cl.ravenhill.keen.genetic.genes.numerical
 import cl.ravenhill.keen.Core
 import cl.ravenhill.keen.genetic.chromosomes.numerical.DoubleChromosome
 import cl.ravenhill.keen.genetic.genes.ComparableGene
+import cl.ravenhill.keen.util.random
 import java.util.Objects
 
 /**
@@ -25,11 +26,7 @@ class DoubleGene(override val dna: Double, private val range: ClosedFloatingPoin
     override fun toDouble() = dna
     override fun toInt() = dna.toInt()
 
-    override fun mutate(): DoubleGene {
-        val max = range.endInclusive
-        val min = range.start
-        return DoubleGene(dna + (Core.rng.nextDouble() * (max - min) + min), range)
-    }
+    override fun mutate() = DoubleGene(range.random(), range)
 
     override fun duplicate(dna: Double) = DoubleGene(dna, range)
 
