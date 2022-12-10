fd3a7c2f924f71ef37a29f0be64440c3e909e4b5
R8V <ignacio.slater@ug.uchile.cl>
2022-12-10 19:31:15 -0300

    DOC Exceptions documentation
    v1.2.2212.4

--- src/main/kotlin/cl/ravenhill/keen/Exceptions.kt
+++ src/main/kotlin/cl/ravenhill/keen/Exceptions.kt
@@ -1,22 +1,34 @@
 package cl.ravenhill.keen
 
+/**
+ * Base exception for all exceptions thrown by Keen.
+ *
+ * @param prefix The prefix to be used in the message.
+ * @param lazyMessage The message to be used in the exception.
+ */
 open class KeenException(prefix: String, lazyMessage: () -> String) :
         Exception("$prefix ${lazyMessage()}")
 
+/**
+ * Exception thrown when the receiver of a function is not valid.
+ */
 class InvalidReceiverException(lazyMessage: () -> String) :
         KeenException("Invalid receiver: ", lazyMessage)
 
+/**
+ * Exception thrown when an argument is not valid.
+ */
 class InvalidArgumentException(lazyMessage: () -> String) :
         KeenException("Invalid argument: ", lazyMessage)
 
-class EngineConfigurationException(lazyMessage: () -> String) :
-        KeenException("Engine configuration error:", lazyMessage)
-
-class GenotypeConfigurationException(lazyMessage: () -> String) :
-        KeenException("Genotype configuration error: ", lazyMessage)
-
+/**
+ * Exception thrown when a limit is not configured correctly.
+ */
 class LimitConfigurationException(lazyMessage: () -> String) :
         KeenException("Genotype configuration error:", lazyMessage)
 
+/**
+ * Exception thrown when a selection operation fails.
+ */
 class SelectorException(lazyMessage: () -> String) :
         KeenException("Selector operation exception:", lazyMessage)
