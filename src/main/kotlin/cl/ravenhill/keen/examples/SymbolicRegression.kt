d33a4d65bcfb11de0df2e5af0e3eebfc506fe4b8
R8V <ignacio.slater@ug.uchile.cl>
2022-12-20 21:45:19 -0300

    Tmp

--- src/main/kotlin/cl/ravenhill/keen/examples/SymbolicRegression.kt
+++ src/main/kotlin/cl/ravenhill/keen/examples/SymbolicRegression.kt
@@ -1,10 +1,10 @@
 package cl.ravenhill.keen.examples
 
+import cl.ravenhill.keen.Builders.engine
+import cl.ravenhill.keen.Builders.genotype
 import cl.ravenhill.keen.Core
 import cl.ravenhill.keen.genetic.Genotype
-import cl.ravenhill.keen.genetic.genes.prog.Program
 import cl.ravenhill.keen.prog.Add
-import cl.ravenhill.keen.prog.Fun
 import cl.ravenhill.keen.prog.Mul
 import cl.ravenhill.keen.prog.Reduceable
 import cl.ravenhill.keen.prog.Sub
@@ -19,10 +19,12 @@ private val terminals = listOf(
     Variable("x", 0),
     Value { Core.rng.nextDouble() * 10 })
 
-private fun fitness(genotype: Genotype<Tree<Reduceable<Double>>>) : Double {
+private fun fitness(genotype: Genotype<Tree<Reduceable<Double>>>): Double {
     TODO()
 }
 
 fun main() {
+    val engine = engine(::fitness, genotype {
 
+    }) {}
 }
