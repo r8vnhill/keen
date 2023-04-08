411ee1d50a99cf16a70d55811b2820130df9131e
R8V <ignacio.slater@ug.uchile.cl>
2023-04-07 21:14:30 -0400

    DOC Improves CharChromosome documentation v2.0.0#2304.24

--- src/main/kotlin/cl/ravenhill/keen/genetic/chromosomes/CharChromosome.kt
+++ src/main/kotlin/cl/ravenhill/keen/genetic/chromosomes/CharChromosome.kt
@@ -5,25 +5,62 @@ import cl.ravenhill.keen.genetic.genes.Gene
 import java.util.*
 
 
+/**
+ * A chromosome that consists of genes representing character values.
+ *
+ * The `CharChromosome` class implements the [Chromosome] interface and represents a collection
+ * of [CharGene] instances.
+ * A `CharChromosome` instance contains a list of character genes within a specified [range] and
+ * [filter] function.
+ * The [withGenes] function returns a new `CharChromosome` instance with the specified list of genes
+ * and the same [range] and [filter] as the original.
+ *
+ * @property genes The list of character genes that make up the chromosome.
+ * @property range The range of possible character values for each gene.
+ * @property filter The function used to filter the possible character values for each gene.
+ *  Defaults to a function that accepts all character values.
+ *
+ * @constructor Creates a new `CharChromosome` instance with the specified list of [genes], [range],
+ *  and [filter].
+ *
+ * @author <a href="https://www.github.com/r8vnhill">R8V</a>
+ * @since 1.0.0
+ * @version 2.0.0
+ */
 class CharChromosome(
     override val genes: List<Gene<Char>>,
+    val range: CharRange,
     private val filter: (Char) -> Boolean = { true }
 ) : Chromosome<Char> {
 
-    override fun withGenes(genes: List<Gene<Char>>) = CharChromosome(genes)
 
+    /**
+     * Creates a new `CharChromosome` instance with a random set of [CharGene] instances.
+     *
+     * @param size The size of the new chromosome.
+     * @param filter The function used to filter the possible character values for each gene.
+     *  Defaults to a function that accepts all character values.
+     * @param range The range of possible character values for each gene.
+     *  Defaults to ``' '..'z'``.
+     */
     constructor(
         size: Int,
         filter: (Char) -> Boolean = { true },
-        range: CharRange
-    ) : this(List(size) { CharGene.create(range, filter) })
+        range: CharRange = ' '..'z'
+    ) : this(List(size) { CharGene.create(range, filter) }, range = ' '..'z')
 
+    // Documentation inherited from Chromosome
+    override fun withGenes(genes: List<Gene<Char>>) = CharChromosome(genes, range, filter)
+
+    // Documentation inherited from Verifiable
     override fun verify() = genes.all { filter(it.dna) }
 
+    // Documentation inherited from Any
     override fun toString(): String {
         return genes.joinToString("")
     }
 
+    // Documentation inherited from Any
     override fun equals(other: Any?) = when {
         this === other -> true
         other !is CharChromosome -> false
@@ -31,14 +68,30 @@ class CharChromosome(
         else -> genes == other.genes
     }
 
+    // Documentation inherited from Any
     override fun hashCode() = Objects.hash(CharChromosome::class, genes)
 
+    /**
+     * A factory for creating [CharChromosome] instances with a random set of genes.
+     *
+     * @property size The size of the new chromosome.
+     *  Defaults to `1`.
+     * @property range The range of possible character values for each gene.
+     * Defaults to ``' '..'z'``.
+     * @property filter The function used to filter the possible character values for each gene.
+     * Defaults to a function that accepts all character values.
+     */
     class Factory : Chromosome.Factory<Char> {
-        var size = 0
+        var size = 1
         var range = ' '..'z'
         var filter: (Char) -> Boolean = { true }
+
+        /**
+         * Creates a new [CharChromosome] instance with the current factory settings.
+         */
         override fun make() = CharChromosome(size, filter, range)
 
+        // Documentation inherited from Any
         override fun toString() = "CharChromosome.Builder { size: $size }"
     }
 }
