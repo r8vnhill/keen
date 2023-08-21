de4decbc70aec3a78992fc6420f04f86eecdebba
R8V <ignacio.slater@ug.uchile.cl>
2023-08-21 13:35:46 -0400

    Add RandomMutator in keen's mutator architecture
    Implemented a new RandomMutator class while converting the existing Mutator into an interface for broader extension in the future. This will allow for more varied types of Mutators such as BitFlipMutator, under the same architecture while ensuring the safeguarding of mutation probability range to be between 0.0 and 1.0. Additionally, the copyright information was updated. This will greatly improve the modularity and extensibility of the mutation functionality within the library.

---
+++ src/main/kotlin/cl/ravenhill/keen/operators/mutator/RandomMutator.kt
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2023, Ignacio Slater M.
+ * BSD Zero Clause License.
+ */
+
+package cl.ravenhill.keen.operators.mutator
+
+import cl.ravenhill.enforcer.Enforcement.enforce
+import cl.ravenhill.enforcer.requirements.DoubleRequirement.BeInRange
+import cl.ravenhill.keen.Core.Dice
+import cl.ravenhill.keen.Population
+import cl.ravenhill.keen.genetic.Genotype
+import cl.ravenhill.keen.genetic.Phenotype
+import cl.ravenhill.keen.genetic.chromosomes.Chromosome
+import cl.ravenhill.keen.genetic.genes.Gene
+import cl.ravenhill.keen.probability
+import cl.ravenhill.keen.util.eq
+
+/**
+ * The mutator operator is responsible for mutating the [Genotype] of the [Phenotype]s in the
+ * [Population].
+ * There are two distinct roles for the mutator:
+ *
+ * - Exploring the search space. This exploration is often slow compared to the crossover,
+ * but in problems where crossover is disruptive this can be an important way to explore the
+ * landscape.
+ *
+ * - Maintaining diversity. Mutation prevents the population from correlating.
+ * Even if most of the search is done by crossover, mutation is still important to provide the
+ * diversity the crossover needs to work.
+ *
+ * The mutation probability is the value that must be optimized.
+ * The optimal value depends on the role mutation plays.
+ * If the mutation is the main exploration mechanism, then the mutation probability should be high.
+ *
+ * @param DNA The type of the DNA
+ * @constructor Creates a new [RandomMutator] with the given [probability]
+ */
+class RandomMutator<DNA, G : Gene<DNA, G>>(
+    override val probability: Double,
+    val geneProbability: Double = 0.5,
+) : Mutator<DNA, G> {
+
+    init {
+        enforce {
+            "The mutation probability must be between 0.0 and 1.0" {
+                probability must BeInRange(0.0..1.0)
+            }
+        }
+    }
+
+    /**
+     * Mutates a chromosome and returns a [MutatorResult] with the mutated chromosome and the
+     * number of mutations.
+     */
+    override fun mutateChromosome(
+        chromosome: Chromosome<DNA, G>,
+    ): MutatorResult<DNA, G, Chromosome<DNA, G>> {
+        val result = chromosome.genes.map { mutateGene(it) }
+        return MutatorResult(
+            chromosome.withGenes(result.map { it.mutated }),
+            result.sumOf { it.mutations }
+        )
+    }
+
+    /**
+     * Mutates a gene and returns a [MutatorResult] with the mutated gene and the number
+     * of mutations.
+     *
+     * The result is defined based in the mutation [probability], the behaviour of this
+     * probability can be defined by three cases:
+     *
+     *  1. The probability is 0.0, then 0 mutations are performed and a copy of the
+     *     original gene is returned.
+     *  2. The probability is 1.0, then 1 mutation is performed and the mutated gene is
+     *     returned.
+     *  3. The probability is between 0.0 and 1.0, then a random number is generated and
+     *     if it is less than the probability, then 1 mutation is performed and the mutated
+     *     gene is returned, otherwise 0 mutations are performed and a copy of the original
+     *     gene is returned.
+     */
+    private fun mutateGene(gene: G) = when {
+        geneProbability eq 0.0 -> MutatorResult(gene)
+        geneProbability eq 1.0 || Dice.probability() < geneProbability ->
+            MutatorResult(gene.mutate(), 1)
+
+        else -> MutatorResult(gene)
+    }
+
+    override fun toString() = "Mutator { " +
+        "probability: $probability }"
+}
