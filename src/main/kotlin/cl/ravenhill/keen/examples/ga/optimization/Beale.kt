fe32af9ae21c7edba562147e27e5b74f940cfa71
R8V <ignacio.slater@ug.uchile.cl>
2023-09-15 16:50:46 -0300

    Add method documentation and update Beale function
    Added detailed comments explaining the Beale function and its usage in genetic algorithm optimization. Also, updated 'createEngine' method for better readability and broader understanding of it use to evolve population. Enhanced documentation is to provide better understanding of the functions and improvements made to increase usability of the genetic algorithm optimization methods.

--- src/main/kotlin/cl/ravenhill/keen/examples/ga/optimization/Beale.kt
+++ src/main/kotlin/cl/ravenhill/keen/examples/ga/optimization/Beale.kt
@@ -9,12 +9,42 @@ import cl.ravenhill.keen.genetic.Genotype
 import cl.ravenhill.keen.genetic.genes.numerical.DoubleGene
 import kotlin.math.pow
 
+/**
+ * Computes the Beale function value for a given genotype.
+ *
+ * The Beale function is a commonly used test function for optimization algorithms
+ * and is defined as:
+ * f(x,y) = (1.5 - x + xy)^2 + (2.25 - x + xy^2)^2 + (2.625 - x + xy^3)^2
+ *
+ * This function calculates and returns the value of the Beale function
+ * for the given genotype's `x` and `y` values.
+ *
+ * @param genotype The genotype representing the solution, which contains genes for x and y values.
+ * @return The computed Beale function value for the given genotype.
+ *
+ * @author <a href="https://www.github.com/r8vnhill">Ignacio Slater M.</a>
+ * @since 2.0.0
+ * @version 2.0.0
+ */
 private fun beale(genotype: Genotype<Double, DoubleGene>): Double {
     val (x, y) = genotype.flatten()
     return (1.5 - x + x * y).pow(2) + (2.25 - x + x * y.pow(2)).pow(2) +
         (2.625 - x + x * y.pow(3)).pow(2)
 }
 
+/**
+ * Initializes an evolutionary computation engine using the Beale function
+ * and a gene range of -4.5 to 4.5. After evolving the population, the result and
+ * output from the last listener attached to the engine are printed.
+ *
+ * The `createEngine` function sets up an evolutionary engine with a given fitness
+ * function and range for the genes. The Beale function serves as the fitness function
+ * in this example.
+ *
+ * @author <a href="https://www.github.com/r8vnhill">Ignacio Slater M.</a>
+ * @since 2.0.0
+ * @version 2.0.0
+ */
 fun main() {
     val engine = createEngine(::beale, -4.5 to 4.5)
     val result = engine.evolve()
