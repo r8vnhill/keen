8863f935a13bf36ef9de92dac7356e1acf36d8e4
R8V <ignacio.slater@ug.uchile.cl>
2023-09-20 15:06:28 -0300

    Added BukinN6 optimization function and updated IntGeneTest unit tests.
    This commit introduces a new optimization function, BukinN6, to the project. This function will be used for genetic algorithms optimization examples. In addition, the commit significantly expands unit tests for IntGeneTest to include equality and hash code checks. This increase in code coverage ensures the robustness and correctness of the IntGene implementation.

---
+++ src/main/kotlin/cl/ravenhill/keen/examples/ga/optimization/BukinN6.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2023, Ignacio Slater M.
+ * 2-Clause BSD License.
+ */
+
+package cl.ravenhill.keen.examples.ga.optimization
+
+import cl.ravenhill.keen.genetic.Genotype
+import cl.ravenhill.keen.genetic.genes.numerical.DoubleGene
+import kotlin.math.pow
+
+private fun bukinN6(genotype: Genotype<Double, DoubleGene>) = genotype.flatten().let { (x, y) ->
+    100 * (y - 0.01 * x.pow(2) + 1).pow(2) + 0.01 * (x + 10).pow(2)
+}
+
+// fun main() {
+//    val engine = createEngine(::bukinN6, -15.0 to -5.0, -3.0 to 3.0)
+//    val result = engine.evolve()
+//    println("Result: $result")
+//    engine.listeners.last().let { "$it" }.let(::println)
+// }
--- src/test/kotlin/cl/ravenhill/keen/genetic/genes/numerical/IntGeneTest.kt
+++ src/test/kotlin/cl/ravenhill/keen/genetic/genes/numerical/IntGeneTest.kt
@@ -7,6 +7,13 @@ package cl.ravenhill.keen.genetic.genes.numerical
 
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import io.kotest.matchers.types.shouldHaveSameHashCodeAs
+import io.kotest.matchers.types.shouldNotHaveSameHashCodeAs
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.arbitrary
+import io.kotest.property.arbitrary.int
+import io.kotest.property.assume
 import io.kotest.property.checkAll
 
 class IntGeneTest : FreeSpec({
@@ -30,5 +37,69 @@ class IntGeneTest : FreeSpec({
                 }
             }
         }
+
+        "equality should" - {
+            with(Arb) {
+                "be reflexive" {
+                    checkAll(intGene(int())) { g ->
+                        g shouldBe g
+                    }
+                }
+
+                "be symmetric" {
+                    checkAll<Int> { i ->
+                        val g1 = IntGene(i)
+                        val g2 = IntGene(i)
+                        g1 shouldBe g2
+                        g2 shouldBe g1
+                    }
+                }
+
+                "be transitive" {
+                    checkAll<Int> {
+                        val g1 = IntGene(it)
+                        val g2 = IntGene(it)
+                        val g3 = IntGene(it)
+                        g1 shouldBe g2
+                        g2 shouldBe g3
+                        g1 shouldBe g3
+                    }
+                }
+            }
+
+            "hash code should" - {
+                "be consistent with equality" {
+                    checkAll<Int> { i ->
+                        val g1 = IntGene(i)
+                        val g2 = IntGene(i)
+                        g1 shouldHaveSameHashCodeAs g2
+                    }
+                }
+
+                "be different for different genes" {
+                    with(Arb) {
+                        checkAll(intGene(int()), intGene(int())) { g1, g2 ->
+                            assume { g1 shouldNotBe g2 }
+                            g1 shouldNotHaveSameHashCodeAs g2
+                        }
+                    }
+                }
+            }
+        }
     }
 })
+
+/**
+ * Creates an arbitrary generator for an `IntGene` using the provided arbitrary generator for
+ * integers.
+ *
+ * @param i The arbitrary generator for integers which will be bound to the `IntGene`.
+ * @return An arbitrary generator that produces instances of `IntGene`.
+ *
+ * @author <a href="https://www.github.com/r8vnhill">Ignacio Slater M.</a>
+ * @since 2.0.0
+ * @version 2.0.0
+ */
+private fun Arb.Companion.intGene(i: Arb<Int>) = arbitrary {
+    IntGene(i.bind())
+}
