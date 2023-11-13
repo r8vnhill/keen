85754dcd96af7c3f86e7d76da1a3418d906958a6
R8V <ignacio.slater@ug.uchile.cl>
2023-11-13 12:50:07 -0300

    Refine EngineTest with added population size test data
    Improved the "can be created" test case in EngineTest.kt by including population size as an additional param. By using `Arb.positiveInt()`, now it generates positive integers as sample population sizes for each test run.
    
    This enhancement ensures the engine creation functionality is thoroughly tested with varied population sizes, making the test more effective and robust.

--- src/test/kotlin/cl/ravenhill/keen/evolution/EngineTest.kt
+++ src/test/kotlin/cl/ravenhill/keen/evolution/EngineTest.kt
@@ -3,14 +3,15 @@ package cl.ravenhill.keen.evolution
 import cl.ravenhill.keen.arbs.genetic.intGenotypeFactory
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.property.Arb
+import io.kotest.property.arbitrary.positiveInt
 import io.kotest.property.checkAll
 
 class EngineTest : FreeSpec({
 
     "An evolution [Engine]" - {
         "can be created" {
-            checkAll(Arb.intGenotypeFactory()) { genotype ->
-
+            checkAll(Arb.intGenotypeFactory(), Arb.positiveInt()) { genotype, populationSize ->
+                // val engine = Engine(genotype, populationSize)
             }
         }
     }
