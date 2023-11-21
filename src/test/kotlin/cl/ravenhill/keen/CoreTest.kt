7ce47cf85d071ff99f816504ad57b707ca2bd26c
R8V <ignacio.slater@ug.uchile.cl>
2023-11-21 19:01:11 -0300

    Reuploaded to replace corrupted files

--- src/test/kotlin/cl/ravenhill/keen/CoreTest.kt
+++ src/test/kotlin/cl/ravenhill/keen/CoreTest.kt
@@ -1,13 +1,28 @@
+/*
+ *  Copyright (c) 2023, Ignacio Slater M.
+ *  2-Clause BSD License.
+ */
+
 package cl.ravenhill.keen
 
 import cl.ravenhill.jakt.exceptions.CompositeException
 import cl.ravenhill.jakt.exceptions.IntConstraintException
+import cl.ravenhill.keen.arbs.random
 import cl.ravenhill.keen.assertions.shouldHaveInfringement
+import cl.ravenhill.keen.prog.Environment
 import io.kotest.assertions.throwables.shouldThrowUnit
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.property.Arb
 import kotlin.random.Random
-
+import io.kotest.property.arbitrary.int
+import io.kotest.property.arbitrary.nonPositiveInt
+import io.kotest.property.arbitrary.positiveInt
+import io.kotest.property.checkAll
+import cl.ravenhill.keen.arbs.uniqueStrings
+import io.kotest.matchers.maps.shouldBeEmpty
+import io.kotest.matchers.maps.shouldContainKey
+import io.kotest.matchers.maps.shouldHaveSize
 
 class CoreTest : FreeSpec({
 
@@ -23,19 +38,16 @@ class CoreTest : FreeSpec({
         }
 
         "can be set to a new Random" {
-            Core.random shouldBe Random.Default
-            repeat(100) {
-                val r = Random(it)
-                Core.random = r
-                Core.random shouldBe r
+            checkAll(Arb.random()) {
+                Core.random = it
+                Core.random shouldBe it
             }
         }
 
         "can generate random numbers" {
-            Core.random shouldBe Random.Default
-            repeat(100) {
-                val r = Random(it)
-                Core.random = Random(it)
+            checkAll<Long> { seed ->
+                val r = Random(seed)
+                Core.random = Random(seed)
                 repeat(100) {
                     r.nextDouble() shouldBe Core.random.nextDouble()
                 }
@@ -51,8 +63,7 @@ class CoreTest : FreeSpec({
 
         "can be set to a positive integer" {
             Core.maxProgramDepth shouldBe Core.DEFAULT_MAX_PROGRAM_DEPTH
-            repeat(100) {
-                val depth = Random.nextInt(1, 100)
+            checkAll(Arb.positiveInt()) { depth ->
                 Core.maxProgramDepth = depth
                 Core.maxProgramDepth shouldBe depth
             }
@@ -60,12 +71,32 @@ class CoreTest : FreeSpec({
 
         "cannot be set to a non-positive integer" {
             Core.maxProgramDepth shouldBe Core.DEFAULT_MAX_PROGRAM_DEPTH
-            repeat(100) {
-                val depth = Random.nextInt(-100, 0)
-                shouldThrowUnit<CompositeException> { Core.maxProgramDepth = depth }
-                    .shouldHaveInfringement<IntConstraintException>(
-                        "The maximum program depth [$depth] must be positive"
-                    )
+            checkAll(Arb.nonPositiveInt()) { depth ->
+                shouldThrowUnit<CompositeException> {
+                    Core.maxProgramDepth = depth
+                }.shouldHaveInfringement<IntConstraintException>("The maximum program depth [$depth] must be positive")
+                Core.maxProgramDepth shouldBe Core.DEFAULT_MAX_PROGRAM_DEPTH
+            }
+        }
+    }
+
+    "The collection of environments" - {
+        "is empty by default" {
+            Core.environments.isEmpty() shouldBe true
+        }
+
+        "can be added to" {
+            Core.environments.isEmpty() shouldBe true
+            checkAll(Arb.uniqueStrings()) { ids ->
+                Core.environments.shouldBeEmpty()
+                ids.forEach {
+                    it to Environment<Any>(it)
+                }
+                Core.environments.shouldHaveSize(ids.size)
+                ids.forEach {
+                    Core.environments shouldContainKey it
+                }
+                Core.environments.clear()
             }
         }
     }
