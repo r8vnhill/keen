cde403470dd13d93697d0770ca374fb88e5da5df
R8V <ignacio.slater@ug.uchile.cl>
2023-11-22 17:42:53 -0300

    SYNC

--- src/test/kotlin/cl/ravenhill/keen/arbs/evolution/Engine.kt
+++ src/test/kotlin/cl/ravenhill/keen/arbs/evolution/Engine.kt
@@ -6,14 +6,30 @@
 
 package cl.ravenhill.keen.arbs.evolution
 
+import cl.ravenhill.keen.arbs.datatypes.mutableList
+import cl.ravenhill.keen.arbs.datatypes.probability
+import cl.ravenhill.keen.arbs.limits.limit
+import cl.ravenhill.keen.arbs.listeners.evolutionListener
+import cl.ravenhill.keen.arbs.operators.selector
+import cl.ravenhill.keen.arbs.optimizer
+import cl.ravenhill.keen.evolution.EvolutionEngine
 import cl.ravenhill.keen.genetic.Genotype
 import cl.ravenhill.keen.genetic.genes.Gene
 import cl.ravenhill.keen.genetic.genes.numerical.IntGene
+import cl.ravenhill.keen.limits.Limit
+import cl.ravenhill.keen.operators.Alterer
+import cl.ravenhill.keen.operators.selector.Selector
+import cl.ravenhill.keen.util.listeners.EvolutionListener
+import cl.ravenhill.keen.util.optimizer.IndividualOptimizer
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.arbitrary
+import io.kotest.property.arbitrary.constant
 import io.kotest.property.arbitrary.double
 import io.kotest.property.arbitrary.element
+import io.kotest.property.arbitrary.int
+import io.kotest.property.arbitrary.list
 import io.kotest.property.arbitrary.next
+import io.kotest.property.arbitrary.pair
 
 /**
  * Provides an arbitrary generator for fitness functions used in genetic algorithms.
@@ -48,9 +64,73 @@ fun Arb.Companion.fitnessFunction() = element(
     }
 )
 
+/**
+ * Provides an arbitrary generator for creating factory instances of [EvolutionEngine] for property-based testing.
+ *
+ * This generator allows for the creation of [EvolutionEngine.Factory] instances with various configurations,
+ * enabling extensive testing of genetic algorithms under different conditions. Each factory instance
+ * can be customized with different components like fitness functions, genotype factories, population
+ * sizes, limits, optimizers, alterers, selectors, survival rates, and listeners.
+ *
+ * ### Usage:
+ * ```
+ * val engineFactoryArb = Arb.evolutionEngineFactory(
+ *     fitnessFunction = Arb.fitnessFunction(),
+ *     genotypeFactory = Arb.intGenotypeFactory(),
+ *     populationSize = Arb.positiveInt(),
+ *     limits = Arb.list(Arb.limit(), 1..3),
+ *     optimizer = Arb.optimizer(),
+ *     alterers = Arb.list(Arb.alterer(), 1..3),
+ *     selectors = Arb.pair(Arb.selector(), Arb.selector()),
+ *     survivalRate = Arb.probability(),
+ *     listeners = Arb.mutableList(Arb.evolutionListener(), 1..3)
+ * )
+ * val engineFactory = engineFactoryArb.bind() // Instance of Engine.Factory
+ * val geneticEngine = engineFactory.build() // Build Engine instance
+ * // Use in genetic algorithm tests
+ * ```
+ *
+ * The flexibility of this approach allows for thorough testing of genetic algorithms across a wide
+ * spectrum of scenarios and configurations.
+ *
+ * @param T The type representing the genetic data or information.
+ * @param G The type of [Gene] associated with the genetic data.
+ * @param fitnessFunction An [Arb] that generates fitness functions.
+ * @param genotypeFactory An [Arb] that generates [Genotype.Factory] instances.
+ * @param populationSize An optional [Arb] that generates population sizes.
+ * @param limits An optional [Arb] that generates a list of [Limit] instances.
+ * @param optimizer An optional [Arb] that generates [IndividualOptimizer] instances.
+ * @param alterers An optional [Arb] that generates a list of [Alterer] instances.
+ * @param selectors An [Arb] generating a pair of [Selector] instances for survivor and offspring selection.
+ * @param survivalRate An optional [Arb] that generates survival rate values.
+ * @param listeners An optional [Arb] that generates a list of [EvolutionListener] instances.
+ *
+ * @return An [Arb] that generates instances of [EvolutionEngine.Factory] with various configurations.
+ */
 fun <T, G> Arb.Companion.evolutionEngineFactory(
     fitnessFunction: Arb<(Genotype<T, G>) -> Double>,
-
+    genotypeFactory: Arb<Genotype.Factory<T, G>>,
+    populationSize: Arb<Int>? = int(1..100),
+    limits: Arb<List<Limit<T, G>>>? = list(limit(), 1..3),
+    optimizer: Arb<IndividualOptimizer<T, G>>? = optimizer(),
+    alterers: Arb<List<Alterer<T, G>>>? = constant(emptyList()),
+    selectors: Arb<Pair<Selector<T, G>?, Selector<T, G>?>> = pair(selector(), selector()),
+    survivalRate: Arb<Double>? = probability(),
+    listeners: Arb<MutableList<EvolutionListener<T, G>>>? = mutableList(evolutionListener(), 1..3),
 ) where G : Gene<T, G> = arbitrary {
-
+    EvolutionEngine.Factory(
+        fitnessFunction.bind(),
+        genotypeFactory.bind()
+    ).apply {
+        populationSize?.let { this.populationSize = it.bind() }
+        limits?.let { this.limits = it.bind() }
+        optimizer?.let { this.optimizer = it.bind() }
+        alterers?.let { this.alterers = it.bind() }
+        selectors.bind().let {
+            it.first?.let { selector -> survivorSelector = selector }
+            it.second?.let { selector -> offspringSelector = selector }
+        }
+        survivalRate?.let { this.survivalRate = it.bind() }
+        listeners?.let { this.listeners = it.bind() }
+    }
 }
--- src/test/kotlin/cl/ravenhill/keen/assertions/Should.kt
+++ src/test/kotlin/cl/ravenhill/keen/assertions/Should.kt
@@ -2,9 +2,11 @@ package cl.ravenhill.keen.assertions
 
 import cl.ravenhill.jakt.exceptions.CompositeException
 import cl.ravenhill.jakt.exceptions.ConstraintException
+import cl.ravenhill.keen.util.DoubleRange
 import io.kotest.matchers.Matcher
 import io.kotest.matchers.MatcherResult
 import io.kotest.matchers.should
+import io.kotest.matchers.shouldNot
 
 /**
  * Enforces that an [CompositeException] is present in the
@@ -31,13 +33,25 @@ inline fun <reified T> CompositeException.shouldHaveInfringement(message: String
                     filtered.any { it.message == message },
                     {
                         "$value should have an infringement of type ${T::class.simpleName} with message: $message. " +
-                              "Actual: $filtered"
+                                "Actual: $filtered"
                     },
                     {
                         "$value should not have an infringement of type ${T::class.simpleName} with message: " +
-                              "$message. Actual: $filtered"
+                                "$message. Actual: $filtered"
                     }
                 )
             }
         }
     )
+
+private fun beInRange(range: DoubleRange) = object : Matcher<Double> {
+    override fun test(value: Double): MatcherResult {
+        return MatcherResult(
+            value in range,
+            { "$value should be in range $range" },
+            { "$value should not be in range $range" }
+        )
+    }
+}
+
+infix fun Double.shouldNotBeInRange(range: DoubleRange) = this shouldNot beInRange(range)
