ea9fdef9e7cc746f117f531488b2910983bbac5b
R8V <ignacio.slater@ug.uchile.cl>
2023-11-24 20:38:35 -0300

    Adds tests for single evolution step

---
+++ src/test/kotlin/cl/ravenhill/keen/assertions/evolution/Evolution.kt
@@ -0,0 +1,40 @@
+/*
+ *  Copyright (c) 2023, Ignacio Slater M.
+ *  2-Clause BSD License.
+ */
+
+
+package cl.ravenhill.keen.assertions.evolution
+
+import cl.ravenhill.keen.Core
+import cl.ravenhill.keen.evolution.EvolutionEngine
+import cl.ravenhill.keen.evolution.EvolutionState
+import cl.ravenhill.keen.genetic.Genotype
+import cl.ravenhill.keen.genetic.Individual
+import cl.ravenhill.keen.genetic.genes.numerical.IntGene
+import io.kotest.core.spec.style.freeSpec
+import io.kotest.matchers.shouldBe
+import kotlin.random.Random
+
+fun `test Engine single evolution step`() = freeSpec {
+    "An Evolution Engine can evolve a population from a given state" - {
+        "when the state is empty" - {
+            "should return a new state with the expected size" {
+                val state = EvolutionState.empty<Int, IntGene>()
+                val engine = EvolutionEngine.Factory<Int, IntGene>({ 1.0 }, Genotype.Factory()).make()
+                val result = engine.evolve(state)
+                result.size shouldBe engine.populationSize
+            }
+
+            "should return a new state with the expected individuals" {
+                Core.random = Random(11)
+                val state = EvolutionState.empty<Int, IntGene>()
+                val engine = EvolutionEngine.Factory<Int, IntGene>({ 1.0 }, Genotype.Factory()).apply {
+                    populationSize = 4
+                }.make()
+                val result = engine.evolve(state)
+                result shouldBe EvolutionState(1, Individual<Int, IntGene>(Genotype(), 1.0), Individual(Genotype(), 1.0), Individual(Genotype(), 1.0), Individual(Genotype(), 1.0))
+            }
+        }
+    }
+}
