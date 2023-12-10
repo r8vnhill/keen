439b8eb20393cd2c824d28948cf68fe9ace65157
R8V <ignacio.slater@ug.uchile.cl>
2023-12-10 13:22:58 -0300

    Revise GeneticMaterial interface description and method documentation
    Updated the documentation of the GeneticMaterial interface to clarify its purpose and functionality in genetic algorithms, focusing on essential operational aspects. Optimized the method documentation by removing redundant explanations, providing succinct overviews and emphasizing the crucial parts of functionalities.

--- core/src/main/kotlin/cl/ravenhill/keen/genetic/GeneticMaterial.kt
+++ core/src/main/kotlin/cl/ravenhill/keen/genetic/GeneticMaterial.kt
@@ -12,52 +12,46 @@ import cl.ravenhill.keen.mixins.Verifiable
 
 
 /**
- * An interface representing the genetic material in a genetic algorithm.
+ * Interface representing the encapsulation of genetic information in a genetic algorithm.
  *
- * This interface extends both `Verifiable` and `MultiStringFormat`, combining the capabilities
- * of verification and multiple string representations. It is specifically designed for use in
- * genetic algorithms, encapsulating genetic information and operations on it.
+ * `GeneticMaterial` serves as a foundational element in genetic algorithms, combining aspects of verification and
+ * diverse string representation capabilities. It's designed for classes that encapsulate genetic data, like genes or
+ * chromosomes, providing essential operations and transformation capabilities for such data.
  *
- * ## Features:
- * - **Verification**: Inherits the `verify` method from `Verifiable`, allowing for custom
- *   verification logic.
- * - **String Representations**: Inherits `toSimpleString` and `toDetailedString` from
- *   `MultiStringFormat`, providing flexibility in how genetic material is represented as a string.
- * - **Flattening**: Provides a method to flatten the genetic material into a list of its basic elements.
+ * ## Key Features:
+ * - **Verification**: Inherits from [Verifiable], enabling implementation of custom verification logic to ensure
+ *   the integrity and validity of the genetic data.
+ * - **String Representation**: Inherits from [MultiStringFormat], offering methods [toSimpleString] and
+ *   [toDetailedString] for versatile textual representation of genetic material.
+ * - **Flattening Genetic Data**: Includes a [flatten] method, which transforms structured genetic material into a list
+ *   of basic elements, facilitating individual-level operations in genetic algorithms.
+ * - **Mapping Elements**: Provides a [flatMap] method, allowing for transformation of the genetic material's elements
+ *   into a list of another type, enhancing the versatility in processing genetic data.
  *
- * ## Usage:
- * This interface is meant to be implemented by classes that represent genetic material, such as
- * chromosomes or genes. Implementors can provide custom logic for verification and string representation,
- * as well as define how the genetic material can be transformed.
+ * ## Applicability:
+ * Ideal for use in classes that model genetic elements, enabling them to align with the operational needs of genetic
+ * algorithms, such as verification, transformation, and diverse representation for analysis or debugging purposes.
  *
- * @param T The type of the genetic data.
- * @param G The specific type of `Gene` that encapsulates the genetic data.
+ * @param T The type of data encapsulated within the genetic material.
+ * @param G The specific gene type, extending `Gene<T, G>`, representing the genetic data.
  */
 interface GeneticMaterial<T, G> : Verifiable, MultiStringFormat where G : Gene<T, G> {
 
     /**
-     * Flattens the genetic material into a list of its basic elements.
+     * Transforms the genetic material into a list of its basic genetic elements.
+     * Useful for processing or evaluating genetic data at the individual element level.
      *
-     * This method provides a way to transform the structured genetic material, such as a chromosome or a collection of
-     * genes, into a flat list of its basic elements (e.g., the individual genetic values). It's particularly useful in
-     * scenarios where operations or evaluations on the genetic algorithm need to be performed at the level of
-     * individual genetic elements, rather than on the structured genetic material as a whole.
-     *
-     * ## Usage:
-     * In the context of a genetic algorithm, this method can be used to access the individual elements of the genetic
-     * material for processes like fitness evaluation, mutation, or crossover.
-     *
-     * ### Example:
-     * ```kotlin
-     * val geneticMaterial: GeneticMaterial<Char, CharGene> = // Some genetic material
-     * val flattenedGenes = geneticMaterial.flatten() // Returns a List<Char> representing individual genes
-     * ```
-     * In this example, `flattenedGenes` will be a list of characters, each representing an individual gene within the
-     * genetic material.
-     *
-     * @return A list of type [T] elements representing the flattened genetic material.
+     * @return A list of type [T], representing the flattened genetic elements.
      */
     fun flatten(): List<T>
 
-    fun <U> flatMap(f: (T) -> U): List<U> = flatten().map(f)
+    /**
+     * Maps each element of the flattened genetic material to a different type, creating a transformed list.
+     * This method extends the functionality of [flatten], enabling custom transformations on genetic elements.
+     *
+     * @param transform A transformation function applied to each flattened element of type [T].
+     * @return A list of transformed elements of type [U].
+     */
+    fun <U> flatMap(transform: (T) -> U): List<U> = flatten().map(transform)
 }
+
