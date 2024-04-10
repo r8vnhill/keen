0af57520552c9406acb2f7f0e81e752f40261445
R8V <ignacio.slater@ug.uchile.cl>
2024-04-10 18:53:51 -0400

    ✨✅ Add Crossover generator to Alterers test
    The test file for the class Alterers has been updated to include a generator for `Crossover<T, G>` instances. This new addition will support property-based testing of crossover operations in evolutionary algorithms. The generator considers parameters such as the number of offspring, parents, chromosome rate, and an exclusivity flag.

--- keen-core/src/test/kotlin/cl/ravenhill/keen/arb/operators/Alterers.kt
+++ keen-core/src/test/kotlin/cl/ravenhill/keen/arb/operators/Alterers.kt
@@ -12,11 +12,14 @@ import cl.ravenhill.keen.genetic.chromosomes.Chromosome
 import cl.ravenhill.keen.genetic.genes.Gene
 import cl.ravenhill.keen.genetic.genes.numeric.IntGene
 import cl.ravenhill.keen.operators.alteration.Alterer
+import cl.ravenhill.keen.operators.alteration.crossover.Crossover
 import cl.ravenhill.keen.operators.alteration.mutation.BitFlipMutator
 import cl.ravenhill.keen.operators.alteration.mutation.Mutator
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.arbitrary
+import io.kotest.property.arbitrary.boolean
 import io.kotest.property.arbitrary.choice
+import io.kotest.property.arbitrary.int
 import io.kotest.property.arbitrary.next
 
 /**
@@ -85,4 +88,53 @@ fun <G> arbBitFlipMutator(
 fun arbAnyMutator(
     individualRate: Arb<Double> = arbProbability(),
     chromosomeRate: Arb<Double> = arbProbability(),
-)= Arb.choice(arbBaseMutator<Int, IntGene>(individualRate, chromosomeRate))
+) = Arb.choice(arbBaseMutator<Int, IntGene>(individualRate, chromosomeRate))
+
+/**
+ * Generates an arbitrary generator for `Crossover<T, G>` instances, suitable for property-based testing of crossover
+ * operations in evolutionary algorithms.
+ *
+ * ## Functionality:
+ * - The generated `Crossover` instances can produce a specified number of offspring from a set number of parents.
+ * - The `chromosomeRate` determines the likelihood of each chromosome being subject to crossover.
+ * - The `exclusivity` flag indicates whether the crossover operation is exclusive, affecting how offspring are
+ * generated.
+ * - The `crossoverChromosomes` method provided in the implementation reverses the genes of each chromosome and takes
+ * the first `numOffspring` chromosomes to form the offspring.
+ *
+ *
+ * @param T The type parameter representing the value type within the gene.
+ * @param G The gene type, constrained to be a subclass of `Gene<T, G>`.
+ * @param numOffspring
+ *  An `Arb<Int>` for generating the number of offspring to be produced by the crossover operation, defaulting to a
+ *  range of 1 to 10.
+ * @param numParents
+ *  An `Arb<Int>` for generating the number of parents involved in the crossover, defaulting to a range of 2 to 10.
+ * @param chromosomeRate
+ *  An `Arb<Double>` for generating the rate at which chromosomes are selected for crossover, using [arbProbability]
+ *  by default.
+ * @param exclusivity
+ *  An `Arb<Boolean>` for generating the exclusivity flag of the crossover operation, defaulting to a random boolean.
+ *
+ * @return An `Arb<Crossover<T, G>>` that produces `Crossover` instances with the specified parameters.
+ */
+fun <T, G> arbBaseCrossover(
+    numOffspring: Arb<Int> = Arb.int(1..10),
+    numParents: Arb<Int> = Arb.int(2..10),
+    chromosomeRate: Arb<Double> = arbProbability(),
+    exclusivity: Arb<Boolean> = Arb.boolean(),
+): Arb<Crossover<T, G>> where G : Gene<T, G> = arbitrary {
+    val nOffspring = numOffspring.bind()
+    val nParents = numParents.bind()
+    val cRate = chromosomeRate.bind()
+    val exclusive = exclusivity.bind()
+    object : Crossover<T, G> {
+        override val numOffspring: Int = nOffspring
+        override val numParents: Int = nParents
+        override val chromosomeRate: Double = cRate
+        override val exclusivity: Boolean = exclusive
+
+        override fun crossoverChromosomes(chromosomes: List<Chromosome<T, G>>) =
+            chromosomes.map { it.duplicateWithGenes(it.reversed()) }.take(nOffspring)
+    }
+}
