7d3430d00e5842ec3ce6c86debfd93b566f3092f
R8V <ignacio.slater@ug.uchile.cl>
2024-04-12 17:09:17 -0400

    ✅ Expand SinglePointCrossoverTest with new test scenarios
    The `SinglePointCrossoverTest` class has been expanded with additional test cases to improve coverage. These new tests check for different scenarios including a custom chromosome rate and the exclusive/non-exclusive nature of the crossover. This will ensure the robustness of the `SinglePointCrossover` operator implementation.

--- keen-core/src/test/kotlin/cl/ravenhill/keen/operators/alteration/crossover/SinglePointCrossoverTest.kt
+++ keen-core/src/test/kotlin/cl/ravenhill/keen/operators/alteration/crossover/SinglePointCrossoverTest.kt
@@ -1,16 +1,59 @@
 package cl.ravenhill.keen.operators.alteration.crossover
 
+import cl.ravenhill.keen.arb.datatypes.arbProbability
 import cl.ravenhill.keen.arb.operators.arbSinglePointCrossover
+import cl.ravenhill.keen.assertions.should.shouldBeExclusive
 import cl.ravenhill.keen.assertions.should.shouldNotBeExclusive
 import cl.ravenhill.keen.genetic.genes.numeric.IntGene
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.constant
+import io.kotest.property.arbitrary.flatMap
+import io.kotest.property.arbitrary.map
 import io.kotest.property.checkAll
 
 class SinglePointCrossoverTest : FreeSpec({
     "A Single Point Crossover Operator can be constructed" - {
         "with default parameters" {
             checkAll(arbSinglePointCrossover<Int, IntGene>(null, null)) { crossover ->
+                with(crossover) {
+                    numParents shouldBe 2
+                    numOffspring shouldBe 2
+                    shouldNotBeExclusive()
+                    chromosomeRate shouldBe 1.0
+                }
+            }
+        }
+
+        "with a custom chromosome rate" {
+            checkAll(
+                arbProbability().flatMap { rate ->
+                    arbSinglePointCrossover<Int, IntGene>(Arb.constant(rate), null)
+                        .map { rate to it }
+                }) { (rate, crossover) ->
+                with(crossover) {
+                    numParents shouldBe 2
+                    numOffspring shouldBe 2
+                    shouldNotBeExclusive()
+                    chromosomeRate shouldBe rate
+                }
+            }
+        }
+
+        "with exclusivity" {
+            checkAll(arbSinglePointCrossover<Int, IntGene>(null, Arb.constant(true))) { crossover ->
+                with(crossover) {
+                    numParents shouldBe 2
+                    numOffspring shouldBe 2
+                    shouldBeExclusive()
+                    chromosomeRate shouldBe 1.0
+                }
+            }
+        }
+
+        "without exclusivity" {
+            checkAll(arbSinglePointCrossover<Int, IntGene>(null, Arb.constant(false))) { crossover ->
                 with(crossover) {
                     numParents shouldBe 2
                     numOffspring shouldBe 2
