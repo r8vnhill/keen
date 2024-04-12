e7dcc4d5ef30f6bab7610bcda47017619ae80fc7
R8V <ignacio.slater@ug.uchile.cl>
2024-04-12 16:53:31 -0400

    ✨✅🔍 Add exclusivity assertions for genetic crossover tests
    Implemented new functionality for testing the exclusivity of crossover mechanisms in genetic algorithms. Added three extension functions to assert if a crossover is exclusive, not exclusive, or to directly test the exclusivity. Adapted the SinglePointCrossover test to utilize these new functions, improving readability and test clarity.

--- keen-core/src/test/kotlin/cl/ravenhill/keen/operators/alteration/crossover/SinglePointCrossoverTest.kt
+++ keen-core/src/test/kotlin/cl/ravenhill/keen/operators/alteration/crossover/SinglePointCrossoverTest.kt
@@ -1,8 +1,8 @@
 package cl.ravenhill.keen.operators.alteration.crossover
 
+import cl.ravenhill.keen.assertions.should.shouldNotBeExclusive
 import cl.ravenhill.keen.genetic.genes.numeric.IntGene
 import io.kotest.core.spec.style.FreeSpec
-import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.shouldBe
 
 class SinglePointCrossoverTest : FreeSpec({
@@ -10,7 +10,7 @@ class SinglePointCrossoverTest : FreeSpec({
         "with default parameters" {
             with(SinglePointCrossover<Int, IntGene>()) {
                 chromosomeRate shouldBe 1.0
-                exclusivity.shouldBeFalse()
+                shouldNotBeExclusive()
                 numParents shouldBe 2
                 numOffspring shouldBe 2
             }
