0618a62913e827fe4dda7811c903d96306554664
R8V <ignacio.slater@ug.uchile.cl>
2024-04-15 13:52:08 -0400

    ✨✅🧪 Add PartialShuffleMutator class and test
    The PartialShuffleMutator class and corresponding test have been added to keen.operators.alteration.mutation. The class verifies that the individual and chromosome mutation rates are respectively within the valid range (0..1), with associated test validating this functionality.

---
+++ keen-core/src/main/kotlin/cl/ravenhill/keen/operators/alteration/mutation/PartialShuffleMutator.kt
@@ -0,0 +1,32 @@
+package cl.ravenhill.keen.operators.alteration.mutation
+
+import cl.ravenhill.jakt.Jakt.constraints
+import cl.ravenhill.jakt.constraints.doubles.BeInRange
+import cl.ravenhill.keen.exceptions.MutatorConfigException
+import cl.ravenhill.keen.genetic.chromosomes.Chromosome
+import cl.ravenhill.keen.genetic.genes.Gene
+
+/**
+ * ## References:
+ * 1. Abdoun, Abouchabaka, and Tajani, “Analyzing the Performance of Mutation Operators to Solve the Travelling
+ *  Salesman Problem.”
+ */
+class PartialShuffleMutator<T, G>(override val individualRate: Double, override val chromosomeRate: Double) :
+        Mutator<T, G> where G : Gene<T, G> {
+
+    init {
+        constraints {
+            "Individual mutation rate must be in the range [0, 1]"(::MutatorConfigException) {
+                individualRate must BeInRange(0.0..1.0)
+            }
+
+            "Chromosome mutation rate must be in the range [0, 1]"(::MutatorConfigException) {
+                chromosomeRate must BeInRange(0.0..1.0)
+            }
+        }
+    }
+
+    override fun mutateChromosome(chromosome: Chromosome<T, G>): Chromosome<T, G> {
+        TODO("Not yet implemented")
+    }
+}
---
+++ keen-core/src/test/kotlin/cl/ravenhill/keen/operators/alteration/mutation/PartialShuffleMutatorTest.kt
@@ -0,0 +1,26 @@
+package cl.ravenhill.keen.operators.alteration.mutation
+
+import cl.ravenhill.jakt.exceptions.CompositeException
+import cl.ravenhill.keen.arb.datatypes.arbInvalidProbability
+import cl.ravenhill.keen.assertions.should.shouldHaveInfringement
+import cl.ravenhill.keen.exceptions.MutatorConfigException
+import cl.ravenhill.keen.genetic.genes.numeric.IntGene
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.double
+import io.kotest.property.checkAll
+
+class PartialShuffleMutatorTest : FreeSpec({
+    "When constructing" - {
+        "should throw an exception if the individual rate is not in 0..1" {
+            checkAll(arbInvalidProbability(), Arb.double()) { invalidProbability, chromosomeRate ->
+                shouldThrow<CompositeException> {
+                    PartialShuffleMutator<Int, IntGene>(invalidProbability, chromosomeRate)
+                }.shouldHaveInfringement<MutatorConfigException>(
+                    "Individual mutation rate must be in the range [0, 1]"
+                )
+            }
+        }
+    }
+})
