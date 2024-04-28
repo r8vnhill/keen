303aa9a11c0baddff7d69d7ce16257f62c387d3d
r8vnhill <reachme@ravenhill.cl>
2024-04-27 22:13:40 -0400

    🔨✅🔧 Refactor DisplacementMutator and add tests
    The DisplacementMutator class has been refactored to include default values for its properties. Additionally, new tests have been added to verify the creation of a DisplacementMutator with default and custom values. The existing tests have also been updated to use the new DisplacementMutator class structure.

--- keen-core/src/main/kotlin/cl/ravenhill/keen/operators/alteration/mutation/DisplacementMutator.kt
+++ keen-core/src/main/kotlin/cl/ravenhill/keen/operators/alteration/mutation/DisplacementMutator.kt
@@ -3,8 +3,14 @@ package cl.ravenhill.keen.operators.alteration.mutation
 import cl.ravenhill.jakt.Jakt.constraints
 import cl.ravenhill.jakt.constraints.doubles.BeInRange
 import cl.ravenhill.keen.exceptions.MutatorConfigException
+import cl.ravenhill.keen.genetic.chromosomes.Chromosome
+import cl.ravenhill.keen.genetic.genes.Gene
 
-class DisplacementMutator(individualRate: Double, chromosomeRate: Double, displacementBoundaryProbability: Double) {
+class DisplacementMutator<T, G>(
+    override val individualRate: Double = DEFAULT_INDIVIDUAL_RATE,
+    override val chromosomeRate: Double = DEFAULT_CHROMOSOME_RATE,
+    val displacementBoundaryProbability: Double = DEFAULT_DISPLACEMENT_BOUNDARY_PROBABILITY
+) : Mutator<T, G> where G : Gene<T, G> {
     init {
         constraints {
             "The individual rate ($individualRate) must be in 0.0..1.0"(::MutatorConfigException) {
@@ -19,5 +25,15 @@ class DisplacementMutator(individualRate: Double, chromosomeRate: Double, displa
                 displacementBoundaryProbability must BeInRange(0.0..1.0)
             }
         }
+    }
+
+    override fun mutateChromosome(chromosome: Chromosome<T, G>): Chromosome<T, G> {
+        TODO("Not yet implemented")
+    }
+
+    companion object {
+        const val DEFAULT_INDIVIDUAL_RATE = 1.0
+        const val DEFAULT_CHROMOSOME_RATE = 1.0
+        const val DEFAULT_DISPLACEMENT_BOUNDARY_PROBABILITY = 0.5
     }
 }
--- keen-core/src/test/kotlin/cl/ravenhill/keen/operators/alteration/mutation/DisplacementMutatorTest.kt
+++ keen-core/src/test/kotlin/cl/ravenhill/keen/operators/alteration/mutation/DisplacementMutatorTest.kt
@@ -2,10 +2,13 @@ package cl.ravenhill.keen.operators.alteration.mutation
 
 import cl.ravenhill.jakt.exceptions.CompositeException
 import cl.ravenhill.keen.arb.datatypes.arbInvalidProbability
+import cl.ravenhill.keen.arb.datatypes.arbProbability
 import cl.ravenhill.keen.assertions.should.shouldHaveInfringement
 import cl.ravenhill.keen.exceptions.MutatorConfigException
+import cl.ravenhill.keen.genetic.genes.numeric.DoubleGene
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.double
 import io.kotest.property.checkAll
@@ -21,7 +24,7 @@ class DisplacementMutatorTest : FreeSpec({
                         Arb.double()
                     ) { rate, chRate, displacementBoundaryProb ->
                         shouldThrow<CompositeException> {
-                            DisplacementMutator(rate, chRate, displacementBoundaryProb)
+                            DisplacementMutator<Double, DoubleGene>(rate, chRate, displacementBoundaryProb)
                         }.shouldHaveInfringement<MutatorConfigException>("The individual rate ($rate) must be in 0.0..1.0")
                     }
                 }
@@ -33,7 +36,7 @@ class DisplacementMutatorTest : FreeSpec({
                         Arb.double()
                     ) { rate, chRate, displacementBoundaryProb ->
                         shouldThrow<CompositeException> {
-                            DisplacementMutator(rate, chRate, displacementBoundaryProb)
+                            DisplacementMutator<Double, DoubleGene>(rate, chRate, displacementBoundaryProb)
                         }.shouldHaveInfringement<MutatorConfigException>("The chromosome rate ($chRate) must be in 0.0..1.0")
                     }
                 }
@@ -45,8 +48,33 @@ class DisplacementMutatorTest : FreeSpec({
                         arbInvalidProbability()
                     ) { rate, chRate, displacementBoundaryProb ->
                         shouldThrow<CompositeException> {
-                            DisplacementMutator(rate, chRate, displacementBoundaryProb)
+                            DisplacementMutator<Double, DoubleGene>(rate, chRate, displacementBoundaryProb)
                         }.shouldHaveInfringement<MutatorConfigException>("The displacement boundary probability ($displacementBoundaryProb) must be in 0.0..1.0")
+                    }
+                }
+            }
+
+            "with default values" - {
+                "should create a mutator with the default values" {
+                    val mutator = DisplacementMutator<Double, DoubleGene>()
+                    mutator.individualRate shouldBe DisplacementMutator.DEFAULT_INDIVIDUAL_RATE
+                    mutator.chromosomeRate shouldBe DisplacementMutator.DEFAULT_CHROMOSOME_RATE
+                    mutator.displacementBoundaryProbability shouldBe
+                            DisplacementMutator.DEFAULT_DISPLACEMENT_BOUNDARY_PROBABILITY
+                }
+            }
+
+            "with custom values" - {
+                "should create a mutator with the custom values" {
+                    checkAll(
+                        arbProbability(),
+                        arbProbability(),
+                        arbProbability()
+                    ) { rate, chRate, displacementBoundaryProb ->
+                        val mutator = DisplacementMutator<Double, DoubleGene>(rate, chRate, displacementBoundaryProb)
+                        mutator.individualRate shouldBe rate
+                        mutator.chromosomeRate shouldBe chRate
+                        mutator.displacementBoundaryProbability shouldBe displacementBoundaryProb
                     }
                 }
             }
