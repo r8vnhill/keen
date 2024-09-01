2dc682d967fb42b37d49f125aba6910754c2f23a
r8vnhill <reachme@ravenhill.cl>
2024-09-01 19:20:36 -0400

    ✨ Add `EvolutionState` empty matchers and related tests
    - 🔍 Implemented `beEmpty`, `shouldBeEmpty`, and `shouldNotBeEmpty` matchers for `EvolutionState`.
    - ✅ Added corresponding tests in `EvolutionStateTest` to verify the emptiness behavior of `EvolutionState`.
    - 🛠️ Introduced `arbPopulation` function in `PopulationTest` for generating arbitrary populations.

--- keen-core/src/commonTest/kotlin/cl/ravenhill/keen/PopulationTest.kt
+++ keen-core/src/commonTest/kotlin/cl/ravenhill/keen/PopulationTest.kt
@@ -13,6 +13,7 @@ import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.int
+import io.kotest.property.arbitrary.list
 import io.kotest.property.arbitrary.map
 import io.kotest.property.arbitrary.next
 import io.kotest.property.checkAll
@@ -28,6 +29,22 @@ class PopulationTest : FreeSpec({
         }
     }
 })
+
+/**
+ * Generates an arbitrary population of individuals for evolutionary algorithms.
+ *
+ * @param T The type of value held by the features within the individuals.
+ * @param F The type of feature, which must extend [Feature].
+ * @param R The type of representation, which must extend [Representation].
+ * @param arbIndividual An [Arb] generator for `Individual` instances, defining the characteristics of each individual
+ *   in the population.
+ * @param size An optional [IntRange] specifying the range for the population size. Defaults to 0..100.
+ * @return An [Arb] generator that produces a list of `Individual<T, F, R>` instances, representing a population.
+ */
+fun <T, F, R> arbPopulation(
+    arbIndividual: Arb<Individual<T, F, R>>,
+    size: IntRange = 0..100
+) where F : Feature<T, F>, R : Representation<T, F> = Arb.list(arbIndividual, size)
 
 /**
  * Generates an arbitrary population of individuals along with their corresponding fitness values.
